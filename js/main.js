cbdd897f256dc6b2a5d76c9b11effa32d45a93ca
m1sf17 <mikesflowers@gmail.com>
2011-03-19 01:26:53 +0100

    Start of login implementation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
 function doLogin()
 {
-   $("<form id='loginform'>Login<br></form>")
-     .append("Username:<input type='text' name='username'><br>")
-     .append("Password:<input type='text' name='password'><br>")
-     .appendTo("body");
+    $("body").html("");
+    
+    $("<form id='loginform'>Login<br></form>")
+      .append("Username:<input type='text' name='username' id='username'>")
+      .append("<span id='usernameerror'></span><br>")
+      .append("Password:<input type='text' name='password' id='password'>")
+      .append("<span id='usernameerror'></span><br>")
+      .appendTo("body");
 
-   $("<div>")
-     .append("<button id='logindiv'>Login</button>")
-     .append("<button id='newuserdiv'>New User</button>")
-     .appendTo("#loginform");
+    $("<div>")
+      .append("<button id='loginbutton'>Login</button>")
+      .append("<button id='newuserbutton'>New User</button>")
+      .appendTo("#loginform");
 
-   $("#logindiv, #newuserdiv").button().css( "font-size", "0.6em" );
-   $("#logindiv").click(function(){
+    $("#loginbutton, #newuserbutton").button().css( "font-size", "0.6em" )
+      .button( "disable" );
+    $("#loginbutton").click(function(){
        $("#loginform input, #loginform button").button( "disable" )
          .button( "refresh" ).attr( "readonly", true );
    });
+
+   var errorStrings = [
+       "Username must be at least 4 characters."
+   ];
+   $("#username, #password").keyup(function(){
+       if( $("#username").val().length < 4 )
+       {
+           $("#usernameerror").html( errorStrings[ 0 ] );
+       }
+       else
+       {
+           $("#usernameerror").html( "" );
+       }
+   }).keyup();
 }
