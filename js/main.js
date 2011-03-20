2581b00d2ddeec363ea4f4f3b382311e0fb9d5e5
m1sf17 <mikesflowers@gmail.com>
2011-03-20 07:14:52 -0700

    Finish up client side login, in theory it will work...

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ function doLogin()
       .append("Password:<input type='password' name='password' " +
               "maxlength=40 id='password'>")
       .append("<span id='passworderror'></span><br>")
+      .append("<span id='emailspan'>E-mail:<input type='text' name='email' " +
+              "maxlength=40 id='email'><span id='emailerror'></span></span>")
       .appendTo("body").filterKeys();
 
     $("<div>")
@@ -17,55 +19,84 @@ function doLogin()
       .append("<button id='newuserbutton'>New User</button>")
       .appendTo("#loginform");
 
+    $("#emailspan").css( "display", "none" );
     $("#loginbutton, #newuserbutton").button().css( "font-size", "0.6em" )
       .button( "disable" );
-    $("#loginbutton").click(function(){
-       $("#loginform input, #loginform button").button( "disable" )
-         .button( "refresh" ).attr( "readonly", true );
-   });
+    $("#loginbutton").click(function( evt ){
+        $("#loginform input, #loginform button").button( "disable" )
+          .button( "refresh" ).attr( "readonly", true );
+        evt.preventDefault();
+    });
+    $("#newuserbutton").click(function( evt ){
+        if( $( "#emailspan").css( "display" ) == "" )
+        {
+            doAjax( "newuser2", {
+                email: $("#email").val()
+            });
+        }
+        else if( $( "#usernameerror" ).val().length == 0 &&
+            $( "#passworderror" ).val().length == 0 )
+        {
+            doAjax( "newuser1", {
+                username: $("#username").val(),
+                password: $("#password").val()
+            });
+        }
+        evt.preventDefault();
+    });
+    $("#loginbutton").click(function( evt ){
+        doAjax( "login", {
+            username: $("#username").val(),
+            password: $("#password").val()
+        });
+        evt.preventDefault();
+    });
 
-   $("#usernameerror, #passworderror").css( "font-weight", "bold" );
+    $("#usernameerror, #passworderror").css( "font-weight", "bold" );
 
-   var errorStrings = [
-       "Username must be at least 4 characters.",
-       "Username cannot start with a number.",
-       "Password must be at least 4 characters."
-   ];
-   $("#username, #password").keyup(function(){
-       var passed = true;
-       if( $("#username").val().substring( 0, 1 ).search( "[0-9]" ) > -1 )
-       {
-           loginError( true, errorStrings[ 1 ] );
-           passed = false;
-       }
-       else if( $("#username").val().length < 4 )
-       {
-           loginError( true, errorStrings[ 0 ] );
-           passed = false;
-       }
-       else
-       {
-           loginError( true, "" );
-       }
-       if( $("#password").val().length < 4 )
-       {
-           loginError( false, errorStrings[ 2 ] );
-           passed = false;
-       }
-       else
-       {
-           loginError( false, "" );
-       }
+    var errorStrings = [
+        "Username must be at least 4 characters.",
+        "Username cannot start with a number.",
+        "Password must be at least 4 characters."
+    ];
+    $("#email").keyup(function(){
+        $("#emailerror").html( "" );
+    })
+    $("#username, #password").keyup(function(){
+        var passed = true;
+        if( $("#username").val().substring( 0, 1 ).search( "[0-9]" ) > -1 )
+        {
+            loginError( true, errorStrings[ 1 ] );
+            passed = false;
+        }
+        else if( $("#username").val().length < 4 )
+        {
+            loginError( true, errorStrings[ 0 ] );
+            passed = false;
+        }
+        else
+        {
+            loginError( true, "" );
+        }
+        if( $("#password").val().length < 4 )
+        {
+            loginError( false, errorStrings[ 2 ] );
+            passed = false;
+        }
+        else
+        {
+            loginError( false, "" );
+        }
 
-       if( passed )
-       {
-           $("#loginform button").button( "enable" );
-       }
-       else
-       {
-           $("#loginform button").button( "disable" );
-       }
-   }).keyup();
+        if( passed )
+        {
+            $("#loginform button").button( "enable" );
+        }
+        else
+        {
+            $("#loginform button").button( "disable" );
+        }
+    }).keyup();
 }
 
 function loginError( isUser, reason )
@@ -85,4 +116,48 @@ function loginError( isUser, reason )
         warnObject.css( "display", "");
         warnObject.html( reason );
     }
+}
+
+function usernameTaken()
+{
+    loginError( true, "Username is already taken." );
+}
+
+function usernameAvailable()
+{
+    $( "#emailspan" ).css( "display", "" );
+    $( "#username, #password" ).attr( "readonly", true );
+}
+
+function emailTaken()
+{
+    $( "#emailerror" ).html( "Email is already associated with an account." );
+}
+
+function accountCreated( id )
+{
+    validLogin( id );
+}
+
+function validLogin( id )
+{
+    //do nothing for now
+    $("body").html( "YAY LOGGED IN WITH " + id + "!" );
+}
+
+function invalidLoginCombo()
+{
+    loginError( true, "Invalid login information." );
+    loginError( false, "Invalid login information." );
+}
+
+function doAjax( outData )
+{
+    $.ajax({
+       url: "ajaxhandler.php",
+       success: function( data ) {
+           eval( data );
+       },
+       data: outData
+    });
 }
