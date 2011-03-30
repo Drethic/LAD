8ff31b2ae738a61b1ba426da93f5783efdacbbb4
m1sf17 <mikesflowers@gmail.com>
2011-03-30 03:58:44 +0200

    Reenable doLogin from layout by setting div to be the layout rather than body

--- js/ui.js
+++ js/ui.js
@@ -2,10 +2,13 @@ function validLogint( id )
 {
     $("head").css('pane', 'display:none;}');
     $("body").html("");
-    $("body").append("<div id='south' class='ui-layout-south'>Taskbar</div>")
-    .append("<div id='east' class='ui-layout-east'>Chat(closeable)</div>")
-    .append("<div id='center' class='ui-layout-center'>Desktop</div>")
-    .layout({
+    $("body").append(
+      $("<div id='layout-container' style='width:100%;height:100%'>")
+        .append("<div id='south' class='ui-layout-south'>Taskbar</div>")
+        .append("<div id='east' class='ui-layout-east'>Chat(closeable)</div>")
+        .append("<div id='center' class='ui-layout-center'>Desktop</div>")
+    );
+    $("#layout-container").layout({
         south: {
            closable: false
         ,  resizable: false
@@ -14,15 +17,13 @@ function validLogint( id )
         ,   east: {
             initClosed: true
         ,   resizable: false
-        }
-    });
+        }});
 
     $("#south")
       .append("<button id='logout'>Logout</button>")
       .css({"background-color" : "#808080"});
 
     $("#logout").click(function( evt ){
-        var url = "index.php";
-        $(location).attr('href',url);
+        doLogin();
     });
 }
