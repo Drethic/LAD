49837719c17eefc635e3df49c20633ff7d90bdcc
m1sf17 <mikesflowers@gmail.com>
2011-04-05 13:06:44 +0200

    Initial commit of programs class for backend

---
+++ private/programs.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Basic concept: Interface to the Programs MySQL table
+ *
+ * Uses:
+ *
+ */
+
+require_once( 'MySqlObject.php' );
+
+class Servers extends MySQLObject
+{
+    function getColumns( )
+    {
+        return array( "ID", "SERVER_ID", "TYPE", "SIZE", "VERSION" );
+    }
+
+    function getIndex( )
+    {
+        return 0;
+    }
+
+    function getTableName( )
+    {
+        return "PROGRAMS";
+    }
+
+    function addProgram( $serverid, $type, $size, $version )
+    {
+        return $this->insert( array( "NULL", $serverid, $type, $size,
+                                     $version ) );
+    }
+
+    function getProgramsByServer( $serverid )
+    {
+        return $this->get( array( 'SERVER_ID' => $serverid ),
+                           array( 'ID' => 'DESC' ) );
+    }
+
+    function getAllPrograms( )
+    {
+        return $this->get( NULL, array( 'ID' => 'DESC' ) );
+    }
+
+    function deleteProgram( $id )
+    {
+        return $this->delete( array( 'ID' => $id ) );
+    }
+}
+
+?>
