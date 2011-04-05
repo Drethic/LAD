3ef0c21a1732b400a4f506c6106f61e2e69898b4
m1sf17 <mikesflowers@gmail.com>
2011-04-05 14:20:54 +0200

    Initial commit of processes backend

---
+++ private/processes.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Basic concept: Interface to the Processes MySQL table
+ *
+ * Uses:
+ *
+ */
+
+require_once( 'MySqlObject.php' );
+
+class Processes extends MySQLObject
+{
+    function getColumns( )
+    {
+        return array( "ID", "TARGET_PROGRAM", "OWNING_SERVER", "CPU_USAGE",
+                      "RAM_USAGE", "BW_USAGE", "OPERATION", "COMPLETION_TIME",
+                      "LINKED_ID" );
+    }
+
+    function getIndex( )
+    {
+        return 0;
+    }
+
+    function getTableName( )
+    {
+        return "PROCESSES";
+    }
+
+    function addProcess( $target, $owner, $cpu, $ram, $bw, $operation,
+                         $completion )
+    {
+        return $this->insert( array( "NULL", $target, $owner, $cpu,
+                                     $ram, $bw, $operation, $completion, 0 ) );
+    }
+
+    function addRemoteProcess( $target, $ownerServer, $targetServer, $ownerCPU,
+                               $targetCPU, $ownerRAM, $targetRAM, $bw,
+                               $operation, $completion )
+    {
+        $id1 = $this->insrt( array( "NULL", $target, $ownerServer, $ownerCPU,
+                                    $ownerRAM, $bw, $operation, $completion,
+                                    0 ) );
+        $id2 = $this->insert( array( "NULL", $target, $targetServer, $targetCPU,
+                                     $targetRAM, $bw, $operation, $completion,
+                                     $id1 ) );
+        $this->update( array( 'LINKED_ID' => $id2), array( 'ID' => $id1 ) );
+        return array( $id1, $id2 );
+    }
+
+    function getProcessesByServer( $serverid )
+    {
+        return $this->get( array( 'OWNING_SERVER' => $serverid ),
+                           array( 'ID' => 'DESC' ) );
+    }
+
+    function getAllProcesses( )
+    {
+        return $this->get( NULL, array( 'ID' => 'DESC' ) );
+    }
+
+    function deleteProcess( $id )
+    {
+        return $this->delete( array( 'ID' => $id ) );
+    }
+}
+
+?>
