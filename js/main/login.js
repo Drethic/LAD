6bc40722bdf0c84008ec2d4f2a688bd63b43d045
m1sf17 <mikesflowers@gmail.com>
2011-05-09 03:58:06 +0300

    I'm lazy...auto focus the username on load

--- js/main/login.js
+++ js/main/login.js
@@ -121,6 +121,8 @@ function doLogin()
             $("#loginform button").button( "disable" );
         }
     }).keyup();
+    
+    $("#username").focus();
 }
 
 function loginError( field, reason )
