1f87ef6192ec1aaed3c39ad36ca71ef9fdc68f57
Joe Robinson <drethic@msn.com>
2016-06-03 20:00:32 -0400

    Basic setup of Bower and Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  var path = require('path');
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    bower: {
+    install: {
+      options: {
+        copy: true,
+        targetDir: './src/libs',
+        layout: function(type, component, source) {
+            var renamedType = type;
+            if (type == 'js') renamedType = 'js';
+            else if (type == 'js/map') renamedType = 'js';
+            else if (type == 'js/lang') renamedType = 'js/lang';
+            else if (type == 'css') renamedType = 'css';
+            else if (type == 'css/img') renamedType = 'css';
+            return path.join(renamedType);;
+          },
+        install: true,
+        verbose: true,
+        prune: false,
+        cleanTargetDir: true,
+        cleanBowerDir: true,
+        bowerOptions: {
+          production: false
+        }
+      }
+    }
+  }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-bower-task');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
